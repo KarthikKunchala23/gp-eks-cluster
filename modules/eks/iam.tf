bf5b1c6f90c9e774bab9c0ae349ba90114c8d51e
KarthikKunchala23 <karthikkunchala2398@gmail.com>
2026-04-25 23:28:05 +0530

    added eks code

---
+++ modules/eks/iam.tf
@@ -0,0 +1,28 @@
+resource "aws_iam_role" "gp-eks-cluster-role" {
+  name = "${var.cluster_name}-role"
+
+  assume_role_policy = <<POLICY
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "eks.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  }
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "gp-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.gp-eks-cluster-role.name
+}
+
+resource "aws_iam_role_policy_attachment" "gp-AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.gp-eks-cluster-role.name
+}
