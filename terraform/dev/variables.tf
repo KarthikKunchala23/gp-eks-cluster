9c2bf14942a544eba0f0386d2f304e20c74b41b0
KarthikKunchala23 <karthikkunchala2398@gmail.com>
2026-04-25 19:13:55 +0530

    fixed variables at root module

--- terraform/dev/variables.tf
+++ terraform/dev/variables.tf
@@ -1,17 +1,17 @@
 variable "vpc_config" {
-  description = "VPC CIDR block details for the EKS cluster"
-  type = object({
+  description = "VPC CIDR block details for the EKS cluster"  
+  default = {
     vpc_cidr = "10.0.0.0/16"
     vpc_name = "gp-eks-vpc"
-  })
+  }
 }
 
 variable "subnet_config" {
   description = "Subnet CIDR block for the EKS cluster"
-  type = object({
-    subnet_cidr = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-    subnet_name = "gp-eks-subnet"
+  default = {
+    subnet_cidr       = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+    subnet_name       = "gp-eks-subnet"
     availability_zone = ["ap-south-1a", "ap-south-1c"]
-    subnet_type = ["public", "public", "private", "private"]  #public/private 
-  })
+    subnet_type       = ["public", "public", "private", "private"]
+  }
 }
