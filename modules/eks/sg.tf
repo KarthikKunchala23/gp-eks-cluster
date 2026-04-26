a2229fa76486f69985e75cb28432fd2f9022a6b1
KarthikKunchala23 <karthikkunchala2398@gmail.com>
2026-04-26 16:03:31 +0530

    fixed sg dependency issue

--- modules/eks/sg.tf
+++ modules/eks/sg.tf
@@ -1,65 +1,57 @@
-# Security group for cluster control plane
 resource "aws_security_group" "eks_cluster_sg" {
-  name        = "${var.cluster_name}-cluster-sg"
-  vpc_id      = var.vpc_id
-  description = "EKS cluster control plane SG"
-
-  # Allow nodes to talk to API server
-  ingress {
-    description     = "Worker nodes to control plane (HTTPS)"
-    from_port       = 443
-    to_port         = 443
-    protocol        = "tcp"
-    security_groups = [aws_security_group.eks_nodes_sg.id]
-  }
-
-  # Outbound to nodes
-  egress {
-    description     = "Control plane to worker nodes"
-    from_port       = 1025
-    to_port         = 65535
-    protocol        = "tcp"
-    security_groups = [aws_security_group.eks_nodes_sg.id]
-  }
+  name   = "${var.cluster_name}-cluster-sg"
+  vpc_id = var.vpc_id
 
   tags = {
-    Name = "${var.cluster_name}-cluster-sg",
-    "kubernetes.io/cluster/${var.cluster_name}" = "owned"
+    Name = "${var.cluster_name}-cluster-sg"
   }
 }
 
 resource "aws_security_group" "eks_nodes_sg" {
-  name        = "${var.cluster_name}-nodes-sg"
-  vpc_id      = var.vpc_id
-  description = "EKS worker nodes SG"
-
-  # Node to node communication
-  ingress {
-    description = "Node to node communication"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    self        = true
-  }
-
-  # Control plane to nodes (kubelet)
-  ingress {
-    description     = "Control plane to nodes"
-    from_port       = 1025
-    to_port         = 65535
-    protocol        = "tcp"
-    security_groups = [aws_security_group.eks_cluster_sg.id]
-  }
-
-  # Outbound internet (for pulling images, etc.)
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  name   = "${var.cluster_name}-nodes-sg"
+  vpc_id = var.vpc_id
 
   tags = {
     Name = "${var.cluster_name}-nodes-sg"
   }
+}
+
+resource "aws_security_group_rule" "nodes_to_cluster" {
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+
+  security_group_id        = aws_security_group.eks_cluster_sg.id
+  source_security_group_id = aws_security_group.eks_nodes_sg.id
+}
+
+resource "aws_security_group_rule" "cluster_to_nodes" {
+  type                     = "ingress"
+  from_port                = 1025
+  to_port                  = 65535
+  protocol                 = "tcp"
+
+  security_group_id        = aws_security_group.eks_nodes_sg.id
+  source_security_group_id = aws_security_group.eks_cluster_sg.id
+}
+
+resource "aws_security_group_rule" "node_to_node" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+
+  security_group_id = aws_security_group.eks_nodes_sg.id
+  self              = true
+}
+
+resource "aws_security_group_rule" "nodes_egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+
+  security_group_id = aws_security_group.eks_nodes_sg.id
+  cidr_blocks       = ["0.0.0.0/0"]
 }
